fb7b49ab731ce9d361b09aa30b6a0244c252b401
azakuanov <azat.zakuanov@yahoo.com>
2016-10-30 16:24:58 -0700

    dz5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -9,6 +9,7 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private  GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private ContactHelper contactHelper;
 
   public void init() {
     wd = new FirefoxDriver();
@@ -17,6 +18,7 @@ public class ApplicationManager {
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
+    contactHelper = new ContactHelper(wd);
     sessionHelper.login("admin", "secret");
   }
 
@@ -32,5 +34,9 @@ public class ApplicationManager {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public ContactHelper getContactHelper() {
+    return contactHelper;
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,32 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class ContactHelper extends HelperBase {
+
+  public ContactHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void returnToHomePage() {
+    click(By.linkText("home page"));
+  }
+
+  public void submitContactCreation() {
+    click(By.xpath("//div[@id='content']/form/input[21]"));
+  }
+
+  public void initContactCreation() {
+    click(By.linkText("add new"));
+  }
+
+  public void fillContactData(ContactData contactData) {
+    type(By.name("firstname"), contactData.getFirstName());
+    type(By.name("middlename"), contactData.getMiddleName());
+    type(By.name("lastname"), contactData.getLastName());
+  }
+}

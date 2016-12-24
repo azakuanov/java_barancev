ab6485bc1a8a29eb796254cacca745b90cec57b2
azakuanov <azat.zakuanov@yahoo.com>
2016-12-24 15:43:53 -0800

    Задание 20

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,24 +1,28 @@
 package ru.stqa.pft.mantis.tests;
 
-
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class TestBase {
 
-  protected static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("brower", BrowserType.CHROME));
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    //app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php","config_inc.php.baсk");
   }
 
-  @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  @AfterSuite (alwaysRun = true)
+  public void tearDown() throws IOException {
+    //app.ftp().restore("config_inc.php.baсk", "config_inc.php");
     app.stop();
   }
 
